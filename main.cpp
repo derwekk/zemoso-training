3ebfb7de786de979ca735682150fa4a655265b5f
Derek Allen <derek.allen@zemosolabs.com>
2023-02-23 09:57:11 +0530

    PR branch

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 //
 //  Created by Derek Allen on 23/02/23.
 //
-
+/*
+ Course Scheduling System: Priya is a member from the training department of a company who has to prepare a training plan that has different courses. Few of these courses need some prerequisite courses to be completed. The goal is to prepare a training plan such that all the courses are included in the correct order and the learning is on track.
+*/
 #include <iostream>
 #include <vector>
 #include <queue>
+
 //{ Driver Code Starts
 using namespace std;
 
